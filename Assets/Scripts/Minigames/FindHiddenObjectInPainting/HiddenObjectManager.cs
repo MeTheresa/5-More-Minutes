cb319c33d6a899b2dd5b382a84898d46dff7def4
BlowFish661 <jessospeeters@gmail.com>
2025-10-06 12:01:25 +0200

    Hidden Objects left text element added

--- Assets/Scripts/Minigames/FindHiddenObjectInPainting/HiddenObjectManager.cs
+++ Assets/Scripts/Minigames/FindHiddenObjectInPainting/HiddenObjectManager.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HiddenObjectManager : MonoBehaviour
 {
     [SerializeField] private List<Button> _hiddenObjects = new List<Button>();
-
+    [SerializeField] private TextMeshProUGUI _objectsLeft;
 
     void Start()
     {
@@ -15,12 +16,16 @@ public class HiddenObjectManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _objectsLeft.text = ("Objects left: " + _hiddenObjects.Count.ToString());
     }
 
     public void HiddenObjectClicked(Button hiddenObject)
     {
-        if(_hiddenObjects.Contains(hiddenObject))
+        if (_hiddenObjects.Contains(hiddenObject))
+        {
             hiddenObject.gameObject.SetActive(false);
+            _hiddenObjects.Remove(hiddenObject);
+        }
+
     }
 }
