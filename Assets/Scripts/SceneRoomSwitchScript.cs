9cfffa5e5d2001d1bf1494a4d2407c3b305a0816
MeTheresa <albertirimia10@gmail.com>
2025-10-06 11:58:47 +0200

    Sound & Scene loading scripts
    - See title lol

---
+++ Assets/Scripts/SceneRoomSwitchScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneRoomSwitchScript : MonoBehaviour
+{
+    //Scene 0 = Prototype
+    //Scene 1 = InteractablePersonTest
+    //Scene 2 = Shaders&Assets
+    //Scene 3 = Test
+    //Scene 4 = FindObjectsInPainting Minigame
+
+    public static void LoadSceneOrMinigame(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
