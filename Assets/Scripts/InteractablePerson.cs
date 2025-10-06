1bdb8c1aa376d6ab7bd3cb4e6194e2e4b2e3f235
cyberpenguin404 <pepijnvanhumbeeck@gmail.com>
2025-10-06 11:37:35 +0200

    Added Interactable Person with dialogue box
    - Changed the Interactable person script to be non abstract
    - Added a test scene with an example implementation of the script

--- Assets/Scripts/InteractablePerson.cs
+++ Assets/Scripts/InteractablePerson.cs
@@ -1,23 +1,62 @@
 using NUnit.Framework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public abstract class InteractablePerson : IInteractable
+public class InteractablePerson : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _name;
-    [SerializeField] private GameObject _visual;
-    [SerializeField] private List<string> _dialogue = new List<string>();
-    private int _dialogueIndex = 0;
-
+    [SerializeField] private Texture2D _visual;
+    [SerializeField] private RawImage _dialogueVisualReference;
+    [SerializeField] private GameObject _dialogueBox;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] public List<string> _dialogue = new List<string>();
+    [SerializeField] private bool _executeOnStart = false;
+    public void Start()
+    {
+        if (_executeOnStart)
+        OnInteract();
+    }
     public virtual void OnInteract()
     {
-        
+        StartCoroutine(HandleDialogue());
+    }
+
+    private void StartDialogue()
+    {
+        _dialogueVisualReference.texture = _visual;
+        _nameText.text = _name;
+        _dialogueBox.SetActive(true);
     }
 
     private IEnumerator HandleDialogue()
     {
+        StartDialogue();
 
+        foreach (string dialogue in _dialogue)
+        {
+            _dialogueText.text = dialogue;
+            yield return new WaitUntil(ButtonInput);
+            yield return new WaitForEndOfFrame();
+        }
+
+        EndDialogue();
+        
         yield return null;
+    }
+
+    private void EndDialogue()
+    {
+        _dialogueBox.SetActive(false);
+    }
+
+    private bool ButtonInput()
+    {
+        return Input.GetMouseButtonUp(0);
     }
 }
---
+++ Assets/Scripts/VictorHorta.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class VictorHorta : InteractablePerson
+{
+
+}
