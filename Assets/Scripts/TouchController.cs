1e4b4d61215cfd6595991269cc1a9c18fe131f50
MiniBot <adam.rozanski@student.howest.be>
2025-10-06 13:57:19 +0200

    Implemented touch input
    Created a public function GetInteractable() using a Raycast to fetch the touched collider.
    Added a Layer for Interactable Objects
    Added a Tag for Doors
    Adjusted Input System Actions to take away mouse false positives.

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,54 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Touch Action Reference")]
+    [SerializeField]
+    private InputActionReference _touchAction;
+    private float _touchDepth = 10f;
+
+    private Vector3 _touchLocation;
+
+    public Vector3 TouchLocation { get => _touchLocation; set { _touchLocation = value; } }
+
+    void Start()
+    {
+        _touchAction.action.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TouchLocationTracker();
+        GetInteractable();
+
+    }
+
+    private Vector3 TouchToWorld(Vector3 TouchCoordinate) //Convert the screen point to world point
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(TouchCoordinate.x, TouchCoordinate.y, _touchDepth));
+    }
+
+    private void TouchLocationTracker() // Update the touch location when the screen is being touched
+    {
+        if (_touchAction.action.triggered)
+        {
+            TouchLocation = TouchToWorld(_touchAction.action.ReadValue<Vector2>());
+        }
+    }
+    private Ray TouchRay() //Get ray from camera to touch location
+    {
+        return new Ray(Camera.main.transform.position, (TouchLocation - Camera.main.transform.position).normalized);
+    }
+
+    public void GetInteractable() //Raycast to get the first interactable gameObject
+    {
+        if (Physics.Raycast(TouchRay(), out RaycastHit hit, Mathf.Infinity))
+            { 
+            Debug.Log("Hit: " + hit.collider.name); 
+            }
+    }
+}
